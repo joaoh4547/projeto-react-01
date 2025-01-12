c4da2a81c2bcde1675d85f95d214da4c6c11c4b6
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-12 17:56:16 -0300

    Capitulo 1 Parte 3

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ export function App() {
 
   return (
     <div>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
-      <Post/>
+      <Post 
+        author="John Due" 
+        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Commodi illo iste vero quisquam ea nam! Possimus eveniet autem, iusto facere, corporis officiis, quasi consequatur veritatis maiores saepe assumenda aspernatur quia?"
+      />
+      <Post 
+        author="John Smith" 
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic rem odio nihil eligendi quibusdam mollitia quam esse voluptas omnis non vero, magnam facere sapiente aut ratione rerum quas totam repudiandae!"
+      />
     </div>
   )
 }
+
 
