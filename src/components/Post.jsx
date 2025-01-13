7deca42c897088222a3c15212436f433c6075c38
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-12 21:24:22 -0300

    Capitulo 2 Parte 7

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -18,13 +18,14 @@ export function Post(props){
                 <time title='12 de Janeiro às 19:35' dateTime="2024-01-12 19:35:45">Publicado há 1h</time>
            </header>
            <div className={styles.content}>
-            <p>Fala galeraa 👋</p>
-
-            <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-            <p>👉 <a href='#'>jane.design/doctorcare</a></p>
-
-            <p><a href='#'>#novoprojeto</a> <a href='#'>#nlw</a> <a href='#'>#rocketseat</a></p>
+                <p>Fala galeraa 👋</p>
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href='#'>jane.design/doctorcare</a></p>
+                <p>
+                    <a href='#'>#novoprojeto</a>{' '}
+                    <a href='#'>#nlw</a>{' '} 
+                    <a href='#'>#rocketseat</a>
+                </p>
            </div>
         </article>
     )
