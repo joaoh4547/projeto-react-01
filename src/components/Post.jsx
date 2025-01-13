6acea668aab35da1ae170869986f9f08fac9190d
João Henrique Teixeira de Jesus <joaohenriquetdj@gmail.com>
2025-01-12 22:10:15 -0300

    Capitulo 2 Parte 9

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment'
 import styles from './Post.module.css'
 
 export function Post(props){
@@ -34,6 +35,11 @@ export function Post(props){
                     <button type="submit">Publicar</button>
                 </footer>
            </form>
+           <div className={styles.commentList}>
+                <Comment/>
+                <Comment/>
+                <Comment/>
+           </div>
         </article>
     )
 }
